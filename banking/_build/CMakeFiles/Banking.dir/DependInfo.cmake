8a0bfbea8b660423efe089861fb9c9309ac71353
AndreyBMWX6 <a_s_bykov@mail.ru>
2020-06-05 19:29:55 +0300

    Add all files

---
+++ banking/_build/CMakeFiles/Banking.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/andrey/AndreyBMWX6/workspace/projects/lab05homework/banking/Account.cpp" "/home/andrey/AndreyBMWX6/workspace/projects/lab05homework/banking/_build/CMakeFiles/Banking.dir/Account.cpp.o"
+  "/home/andrey/AndreyBMWX6/workspace/projects/lab05homework/banking/Transaction.cpp" "/home/andrey/AndreyBMWX6/workspace/projects/lab05homework/banking/_build/CMakeFiles/Banking.dir/Transaction.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ banking/_build/CMakeFiles/Banking.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Banking.dir/Account.cpp.o"
+  "CMakeFiles/Banking.dir/Transaction.cpp.o"
+  "libBanking.a"
+  "libBanking.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Banking.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
