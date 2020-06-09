0d3f1d021994e11cda4718d79a69ea575a265c94
AndreyBMWX6 <a_s_bykov@mail.ru>
2020-06-09 16:26:12 +0300

    Added Hunter

---
+++ _builds/CMakeFiles/check.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/andrey/AndreyBMWX6/workspace/projects/lab07homework/tests/test.cpp" "/home/andrey/AndreyBMWX6/workspace/projects/lab07homework/_builds/CMakeFiles/check.dir/tests/test.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../banking"
+  "/home/andrey/projects/hunter/_Base/xxxxxxx/9b2c9d4/960678b/Install/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/andrey/AndreyBMWX6/workspace/projects/lab07homework/_builds/banking/CMakeFiles/banking.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ _builds/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: /home/andrey/AndreyBMWX6/workspace/projects/lab07homework
+# Build directory: /home/andrey/AndreyBMWX6/workspace/projects/lab07homework/_builds
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(check "/home/andrey/AndreyBMWX6/workspace/projects/lab07homework/_builds/check")
+set_tests_properties(check PROPERTIES  _BACKTRACE_TRIPLES "/home/andrey/AndreyBMWX6/workspace/projects/lab07homework/CMakeLists.txt;27;add_test;/home/andrey/AndreyBMWX6/workspace/projects/lab07homework/CMakeLists.txt;0;")
+subdirs("banking")
